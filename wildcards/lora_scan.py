8b84c86bf025a87e5883f75cef3c9475ccf5be6c
Yak! <yak1ex@users.noreply.github.com>
2025-01-23 10:53:26 +0900

    [lora_scan] Follow style guide.

--- wildcards/lora_scan.py
+++ wildcards/lora_scan.py
@@ -2,18 +2,21 @@ import struct
 import json
 from pathlib import Path
 
+
 def read_from_safetensors(inpath):
     with inpath.open('rb') as infile:
         bytes = infile.read(8)
         header_bytes = (struct.unpack('<Q', bytes))[0]
         header = infile.read(header_bytes).decode('utf-8')
-        data=json.loads(header)
+        data = json.loads(header)
         return data['__metadata__']
+
 
 def read_from_json(inpath):
     with inpath.open() as infile:
-        data=json.loads(infile.read())
+        data = json.loads(infile.read())
         return data
+
 
 readers = {
     'safetensors': read_from_safetensors,
@@ -35,15 +38,17 @@ normalize_map = {
     'SD1': 'sd15'
 }
 
+
 def get_base_model(metadata):
-    if type(metadata) != list:
-        metadata=[metadata]
+    if isinstance(metadata, list):
+        metadata = [metadata]
     for key in base_model_keys:
         for data in metadata:
             if key in data:
                 if data[key] in normalize_map:
                     return normalize_map[data[key]]
     return 'unkn'
+
 
 basename = 'age_slider_v20'
 result = []
